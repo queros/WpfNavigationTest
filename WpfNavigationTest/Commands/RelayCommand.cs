c5074e865bc5703cb0b76cfda2b47354d61b101b
mariusz.kuberczyk <mariusz.kuberczyk@gorrion.pl>
2019-03-28 00:59:06 +0100

    added command, helpers and base view model interface

---
+++ WpfNavigationTest/Commands/RelayCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Input;
+using WpfNavigationTest.Helpers;
+
+namespace WpfNavigationTest.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        public bool CanExecute(object parameter)
+            => _canExecute == null || _canExecute((T)parameter);
+
+        public void Execute(object parameter)
+            => _execute((T)parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public bool CanExecute(object parameter)
+            => _canExecute == null || _canExecute((object)parameter);
+
+        public void Execute(object parameter)
+            => _execute((object)parameter);
+
+        // Ensures WPF commanding infrastructure asks all RelayCommand objects whether their
+        // associated views should be enabled whenever a command is invoked 
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                CanExecuteChangedInternal += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                CanExecuteChangedInternal -= value;
+            }
+        }
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChangedInternal.Raise(this);
+        }
+    }
+}
---
+++ WpfNavigationTest/Helpers/EventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WpfNavigationTest.Helpers
+{
+    public class EventArgs<T> : EventArgs
+    {
+        public EventArgs(T value)
+            => Value = value;
+
+        public T Value { get; private set; }
+    }
+}
